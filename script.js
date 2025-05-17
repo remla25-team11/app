387df3c92e68e16801bb52d0138b7785c32d29a6
Sowmya Prakash <S.Prakash-7@student.tudelft.nl>
2025-05-17 19:34:59 +0200

    change in script js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const API_BASE_URL = window.APP_SERVICE_URL || 'http://localhost:5000';
+const API_BASE_URL = window.APP_SERVICE_URL || '/api';
 console.log("API_BASE_URL = ", API_BASE_URL);
 
 const ENDPOINTS = {
     appVersion: '/version',
     modelVersion: '/model/version',
-    analyze: '/analyze'
+    analyze: '/analyze',
+    feedback: '/feedback'
 };
 
 // DOM Elements
